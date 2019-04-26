b7f0339f06a742f9c2cdf8a1325fb3a1fde49088
Becky <rebeccajoynielsen@gmail.come>
2019-04-26 12:54:47 +0100

    integration spec tests complete

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -15,28 +15,51 @@ describe('calculator', function () {
   it('should be able to add numbers', function(){
     calculator.previousTotal = 4
     calculator.add(1)
-    actual = calculator.runningTotal;
+    const actual = calculator.runningTotal;
     assert.strictEqual(actual, 5);
   })
 
   it('should be able to subtract numbers', function(){
     calculator.previousTotal = 7
     calculator.subtract(4)
-    actual = calculator.runningTotal;
+    const actual = calculator.runningTotal;
     assert.strictEqual(actual, 3)
   })
 
   it('should be able to multiply numbers', function(){
     calculator.previousTotal = 3
     calculator.multiply(5)
-    actual = calculator.runningTotal;
+    const actual = calculator.runningTotal;
     assert.strictEqual(actual, 15)
   })
 
   it('should be able to divide numbers', function(){
     calculator.previousTotal = 21
     calculator.divide(7)
-    actual = calculator.runningTotal;
+    const actual = calculator.runningTotal;
     assert.strictEqual(actual, 3)
+  })
+
+// integration tests
+
+  it('should be able to concatenate multiple number clicks', function(){
+    calculator.numberClick(1);
+    calculator.numberClick(3);
+    const actual = calculator.runningTotal
+    assert.strictEqual(actual, 13)
+  })
+
+  it('should be able to chain multiple operations together', function(){
+    calculator.previousOperator = '+'
+    calculator.operatorClick('-')
+    const actual = calculator.previousOperator
+    assert.strictEqual(actual, '-')
+  })
+
+  it('should be able to clear the running total without affecting the calculation', function(){
+    calculator.runningTotal = 15;
+    calculator.clearClick();
+    const actual = calculator.runningTotal
+    assert.strictEqual(actual, 0)
   })
 });
